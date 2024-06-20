cc2e5717a8d775539de85dfea2bcf4ecdc8dad9c
UsamaAkifHossainAdor <140923439+UsamaAkifHossainAdor@users.noreply.github.com>
2024-06-20 15:52:31 +0600

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+    int main () {
+        int num1;
+        float num2;
+        printf("Enter an integer & floting : ");
+        scanf("%d %f",&num1,&num2);
+
+
+ printf("Numbers are : %d %f\n",num1,num2);
+
+        return 0;
+    }
